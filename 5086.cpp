1e3d6534b9bf82a4a404552bcae0786c4fdef63e
22youl <101039299+22youl@users.noreply.github.com>
2025-04-08 19:44:44 +0900

    Create 5086.cpp

---
+++ 5086.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	while (cin >> a >> b)
+	{
+		if (a == 0 && b == 0)
+			return 0;
+		if (a < b)
+		{
+			if (b % a == 0)
+			{
+				cout << "factor" << endl;
+			}
+			else
+				cout << "neither" << endl;
+		}
+		if (a > b)
+		{
+			if (a % b == 0)
+			{
+			cout << "multiple" << endl;
+			}
+			else
+				cout << "neither" << endl;
+		}
+	}
+	return 0;
+}
