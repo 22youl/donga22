c7575d1ce7c9d2b492f08bfa1e1d64483b03cf88
22youl <101039299+22youl@users.noreply.github.com>
2025-04-21 15:29:53 +0900

    Title: 기수 정렬 구현, Time: 35ms, Memory: 1MB, Status: Passed - Codetree

--- 250421/기수 정렬 구현/implement-radix-sort.cpp
+++ 250421/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
+#include <vector>
+
+#define MAX_N 100000
+#define MAX_K 6
+#define MAX_DIGIT 10
 
 using namespace std;
 
+// 변수 선언
 int n;
-int arr[100];
+int arr[MAX_N];
 
-void ps(int arr[],int n)
-{
-    for(int i=0;i<n-1;i++)
-    {
-        int minimum=i;
-        for(int k=i+1;k<n;k++)
-        {
-            if(arr[k]<arr[minimum])
-            {
-                minimum=k;
-            }
+void RadixSort() {
+    int p = 1;
+    for(int pos = 0; pos < MAX_K; pos++) {
+        vector<int> arr_new[MAX_DIGIT];
+        for(int i = 0; i < n; i++) {
+            int digit = (arr[i] / p) % 10;
+            arr_new[digit].push_back(arr[i]);
         }
-        int tmp=arr[i];
-        arr[i]=arr[minimum];
-        arr[minimum]=tmp;
+
+        int index = 0;
+        for(int i = 0; i < MAX_DIGIT; i++)
+            for(int j = 0; j < (int) arr_new[i].size(); j++)
+                arr[index++] = arr_new[i][j];
+        
+        p *= 10;
     }
 }
+
 int main() {
+    // 입력
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         cin >> arr[i];
-    }
-    ps(arr,n);
-    for(int i=0;i<n;i++)
-    cout << arr[i] << " ";
+    
+    RadixSort();
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
